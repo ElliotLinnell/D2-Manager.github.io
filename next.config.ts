3163b41756050022d651f0672ee08506e79df1ea
Elliot Linnell <Linnellelliot@gmail.com>
2025-02-11 16:15:07 +0000

    boink

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+  experimental: {},
 };
 
 export default nextConfig;
