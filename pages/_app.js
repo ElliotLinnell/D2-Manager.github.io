bd12a315a53b5ab923f629f0630b89bf8e2751c4
Elliot Linnell <Linnellelliot@gmail.com>
2025-02-12 09:49:54 +0000

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <SessionProvider session={pageProps.session}>
+    <Component {...pageProps} />
+    </SessionProvider>
+  );
+
 }
